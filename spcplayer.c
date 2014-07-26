3e3ae0180a16e35e68038e13ca4b418c2a5d91fb
bcharron <bcharron@pobox.com>
2014-07-26 00:42:48 -0400

    Some progress on CPU emulation

--- spcplayer.c
+++ spcplayer.c
@@ -30,6 +30,7 @@
 */
 
 #include <stdio.h>
+#include <assert.h>
 #include <sys/time.h>
 #include <errno.h>
 #include <arpa/inet.h>
@@ -38,11 +39,26 @@
 #include "opcodes.h"
 
 #define SPC_HEADER_LEN 33
-#define SPC_HEADER_CONTAINS_ID_TAG
+#define SPC_TAG_TYPE_OFFSET 0x23
 #define SPC_VERSION_OFFSET 0x24
+#define SPC_ID_TAG_OFFSET 0x2e
 #define SPC_RAM_OFFSET 0x0100
+
 #define SPC_DSP_REGISTERS 128
 #define SPC_RAM_SIZE 65536
+#define SPC_HEADER_MAGIC "SNES-SPC700 Sound File Data v0.30"
+#define SPC_HAS_ID_TAG 26
+
+#define SPC_TAG_SONG_TITLE_LEN 32
+#define SPC_TAG_GAME_TITLE_LEN 32
+#define SPC_TAG_DUMPER_NAME_LEN 32
+#define SPC_TAG_COMMENTS_LEN 32
+
+#define NO_OPERAND 0
+
+/* Passed to functions that may or not update flags */
+#define DONT_ADJUST_FLAGS 0
+#define ADJUST_FLAGS 1
 
 typedef union spc_flags_u {
 	struct {
@@ -86,6 +102,7 @@ typedef struct spc_file_s {
 	Uint8 dsp_registers[128];
 	Uint8 unused[64];
 	Uint8 extra_ram[64];
+	id_tag_t id_tag;
 } spc_file_t;
 
 typedef struct spc_state_s {
@@ -94,6 +111,13 @@ typedef struct spc_state_s {
 	Uint8 *dsp_registers;
 	long cycle;
 } spc_state_t;
+
+int dump_instruction(Uint16 pc, Uint8 *ram);
+void dump_registers(spc_registers_t *registers);
+int execute_next(spc_state_t *state);
+spc_file_t *read_spc_file(char *filename);
+Uint16 get_direct_page_addr(spc_state_t *state, Uint16 addr);
+Uint8 get_direct_page_byte(spc_state_t *state, Uint16 addr);
 
 char *flags_str(spc_flags_t flags)
 {
@@ -116,13 +140,38 @@ char *flags_str(spc_flags_t flags)
 	return(buf);
 }
 
+/* Make a 16-bit value out of two 8-bit ones */
+uint16_t make16(uint8_t high, uint8_t low) {
+	uint16_t offset = ((uint16_t) high << 8) | low;
+
+	return(offset);
+}
+
+// XXX: This is dumb. We need a table sorted by opcode rather than mnemonic..
+opcode_t *get_opcode_by_value(Uint8 opcode) {
+	opcode_t *ret = NULL;
+	int x;
+
+	for (x = 0; x < OPCODE_TABLE_LEN; x++) {
+		if (OPCODE_TABLE[x].opcode == opcode) {
+			ret = &OPCODE_TABLE[x];
+			break;
+		}
+	}
+
+	assert(ret != NULL);
+
+	return(ret);
+}
+
+
 void do_beq(spc_state_t *state, Uint8 operand1)
 {
-	printf("BEQ 0x%02X\n", operand1);
+	// printf("BEQ 0x%02X\n", operand1);
 
 	if (state->regs->psw.f.z) {
 		state->cycle += 2;
-		state->regs->pc += (Sint8) operand1;
+		state->regs->pc += (Sint8) operand1 + 2;
 		printf("Jumping to 0x%04X\n", state->regs->pc);
 	} else {
 		state->regs->pc += 2;
@@ -138,6 +187,7 @@ void instr_or(spc_state_t *state, Uint8 *operand1, Uint8 operand2)
 	state->regs->psw.f.z = (*operand1 == 0);
 }
 
+/*
 void do_or(spc_state_t *state, Uint8 opcode)
 {
 	Uint16 operand16;
@@ -156,6 +206,277 @@ void do_or(spc_state_t *state, Uint8 opcode)
 			instr_or(state, &state->regs->a, state->ram[state->regs->x]);
 			break;
 	}
+}
+*/
+
+/*
+void do_and(spc_state_t *state, Uint8 opcode, Uint8 operand1, Uint8 operand2) {
+	Uint8 val;
+	opcode_t *opcode_ptr;
+
+	switch(opcode) {
+		case 0x24: // ANDZ A,$xx
+			val = get_direct_page_byte(state, operand1);
+			state->regs->a &= val;
+			state->cycle += 2;
+			break;
+
+		default:
+			fprintf(stderr, "Invalid opcode, %02X\n", opcode);
+			break;
+	}
+
+	// state->regs->pc += OPCODE_TABLE[opcode].len;
+	opcode_ptr = get_opcode_by_value(opcode);
+	state->regs->pc += opcode_ptr->len;
+}
+*/
+
+/*
+void instr_mov(spc_state_t *state, Uint8 *dst, Uint8 src, int adjust_flags)
+{
+	*dst = src;
+
+	state->regs->psw.f.n = (*dst & 0x80) > 0;
+	state->regs->psw.f.z = (*dst == 0);
+}
+*/
+
+/*
+void do_mov(spc_state_t *state, Uint8 opcode, Uint8 operand1, Uint8 operand2) {
+	Uint8 val;
+	Uint16 addr;
+	opcode_t *opcode_ptr;
+
+	switch(opcode) {
+		case 0x7D: // MOV A,X
+			instr_mov(state, &state->regs->a, state->regs->x, ADJUST_FLAGS);
+			state->cycle += 2;
+			break;
+
+		case 0xE4: // MOVZ A, $xx
+			// state->regs->a = state->ram[operand1];		
+			val = get_direct_page_byte(state, operand1);
+			instr_mov(state, &state->regs->a, val, ADJUST_FLAGS);
+			state->cycle += 4;
+			break;
+
+		case 0xF5: // MOV A, $xxxx + X
+			addr = make16(operand2, operand1);
+			addr += state->regs->x;
+			instr_mov(state, &state->regs->a, state->ram[addr], ADJUST_FLAGS);
+			state->cycle += 5;
+			break;
+
+		case 0xFD: // MOV Y, A
+			instr_mov(state, &state->regs->y, state->regs->a, ADJUST_FLAGS);
+			state->cycle += 2;
+			break;
+
+		case 0xDA: // MOVW $xx, YA
+			addr = get_direct_page_addr(state, operand1);
+
+			instr_mov(state, &state->ram[addr], state->regs->y, DONT_ADJUST_FLAGS);
+			instr_mov(state, &state->ram[addr + 1], state->regs->a, DONT_ADJUST_FLAGS);
+			state->cycle += 4;	// XXX: One source says 4, another 5..
+			break;
+
+		default:
+			fprintf(stderr, "Invalid opcode, %02X\n", opcode);
+			break;
+	}
+
+	opcode_ptr = get_opcode_by_value(opcode);
+
+	state->regs->pc += opcode_ptr->len;
+}
+*/
+
+Uint8 do_sbc(spc_state_t *state, Uint8 dst, Uint8 operand) {
+	Uint16  result;
+	Uint16 sResult;
+	Uint8 ret;
+
+	result = dst - operand - (! state->regs->psw.f.c);
+	sResult = (Uint8) dst - (Uint8) operand - (! state->regs->psw.f.c);
+	ret = result & 0x00FF;
+
+	state->regs->psw.f.c = !(result > 0xFF);
+	state->regs->psw.f.n = ((ret & 0x80) != 0);
+	state->regs->psw.f.v = (sResult < -128 || sResult > 127);
+	state->regs->psw.f.z = (ret == 0);
+
+	return(ret);
+}
+
+/* Flag 'P' can change if $00 means $0000 or $0100 */
+Uint16 get_direct_page_addr(spc_state_t *state, Uint16 addr) {
+	Uint16 base = 0x0000;
+	Uint16 ret;
+
+	if (state->regs->psw.f.p)
+		base = 0x0100;
+
+	ret = addr + base;
+
+	return(ret);
+}
+
+Uint8 get_direct_page_byte(spc_state_t *state, Uint16 addr) {
+	Uint8 ret;
+
+	Uint16 real_addr = get_direct_page_addr(state, addr);
+	ret = state->ram[real_addr];
+
+	return(ret);
+}
+
+/* Adjust Zero and Negative flag based on value in 'val' */
+void adjust_flags(spc_state_t *state, Uint16 val) {
+	state->regs->psw.f.n = (val & 0x80) > 0;
+	state->regs->psw.f.z = (val == 0);
+}
+
+int execute_instruction(spc_state_t *state, Uint16 addr) {
+	Uint16 dp_addr;
+	Uint16 abs_addr;
+	Uint8 opcode;
+	Uint8 operand1;
+	Uint8 operand2;
+	Uint8 val;
+	opcode_t *opcode_ptr;
+	int cycles = 0;
+
+	// dump_registers(state->regs);
+	// dump_instruction(addr, state->ram);
+
+	opcode = state->ram[addr];
+	operand1 = state->ram[addr + 1];
+	operand2 = state->ram[addr + 2];
+
+	opcode_ptr = get_opcode_by_value(opcode);
+
+	switch(opcode) {
+		case 0x24: // ANDZ A, $xx
+			val = get_direct_page_byte(state, operand1);
+			state->regs->a &= val;
+			cycles = 2;
+			break;
+
+		case 0x5C: // LSR A
+			state->regs->psw.f.c = state->regs->a & 0x01;
+			state->regs->a = state->regs->a >> 1;
+			adjust_flags(state, state->regs->a);
+			cycles = 2;
+			break;
+
+		case 0x7D: // MOV A, X
+			state->regs->a = state->regs->x;
+			adjust_flags(state, state->regs->a);
+			cycles = 2;
+			break;
+
+		case 0x80: // SETC
+			state->regs->psw.f.c = 1;
+			break;
+
+		case 0x9F: // XCN A
+			state->regs->a = ((state->regs->a << 4) & 0xF0) | (state->regs->a >> 4);
+			adjust_flags(state, state->regs->a);
+			cycles = 5;
+			break;
+
+		case 0xB6: // SBC A, $xx + Y
+			abs_addr = make16(operand2, operand1);
+			abs_addr += state->regs->y;
+
+			val = state->ram[abs_addr];
+			
+			state->regs->a = do_sbc(state, state->regs->a, val);
+			// state->regs->a = state->regs->a - val - state->regs->psw.f.c;
+			break;
+
+		case 0xC4: // MOVZ $xx, A
+			dp_addr = get_direct_page_addr(state, operand1);
+			state->ram[dp_addr] = state->regs->a;
+			cycles = 4;
+			break;
+
+		case 0xE4: // MOVZ A, $xx
+			val = get_direct_page_byte(state, operand1);
+			state->regs->a = val;
+			adjust_flags(state, state->regs->a);
+			cycles = 3;
+			break;
+
+		case 0xEB: // MOV Y, $xx
+			val = get_direct_page_byte(state, operand1);
+			state->regs->y = val;
+			adjust_flags(state, state->regs->y);
+			cycles = 3;
+			break;
+
+		case 0xDA: // MOVW $xx, YA
+			dp_addr = get_direct_page_addr(state, operand1);
+
+			state->ram[dp_addr] = state->regs->y;
+			state->ram[dp_addr + 1] = state->regs->a;	// XXX: Is it the other way around?
+			cycles = 4;	// XXX: One source says 4, another 5..
+			break;
+
+		case 0xF0: // BEQ
+			do_beq(state, state->ram[addr + 1]);
+			break;
+
+		case 0xF5: // MOV A, $xxxx + X
+			abs_addr = make16(operand2, operand1);
+			abs_addr += state->regs->x;
+			state->regs->a = state->ram[abs_addr];
+			adjust_flags(state, state->regs->a);
+			cycles = 5;
+			break;
+
+		case 0xF6: // MOV A, $xxxx + Y
+			abs_addr = make16(operand2, operand1);
+			abs_addr += state->regs->y;
+			state->regs->a = state->ram[abs_addr];
+			adjust_flags(state, state->regs->a);
+			cycles = 5;
+			break;
+			
+		case 0xFD: // MOV Y, A
+			state->regs->y = state->regs->a;
+			adjust_flags(state, state->regs->y);
+			cycles = 2;
+			break;
+
+		default:
+			fprintf(stderr, "Instruction #$%02X at $%04X not implemented\n", opcode, addr);
+			exit(1);
+			break;
+	}
+
+	/* Increment PC if not a branch */
+	switch(opcode) {
+		case 0xF0:
+			// PC already modified accordingly.
+			break;
+
+		default:
+			state->regs->pc += opcode_ptr->len;
+			break;
+	}
+
+	state->cycle += cycles;
+
+	return(0);
+}
+
+int execute_next(spc_state_t *state) {
+
+	execute_instruction(state, state->regs->pc);
+
+	return(0);
 }
 
 void dump_registers(spc_registers_t *registers)
@@ -231,11 +552,22 @@ int dump_instruction(Uint16 pc, Uint8 *ram)
 	switch(opcode) {
 		case 0xF0: // BEQ
 		{
-			// No idea why this is +2 rather than +1 or +0. I guess the operand is
-			// part of the pc increment?
+			// +2 because the operands have been read when the CPU gets ready to jump
 			printf(" ($%04X)", pc + ram[pc + 1] + 2);
 			break;
 		}
+
+		/*
+		case 0xF5: // MOV A, $xxxx + X
+		{
+			Uint16 addr = make16(ram[pc + 2], ram[pc + 1]);
+			Uint8 h = ram[addr + 1];
+			Uint8 l = ram[addr];
+			Uint16 final_addr = make16(h, l);
+			printf(" ($%04X + X)", final_addr);
+		}
+		*/
+		break;
 
 		default:
 			break;
@@ -277,6 +609,7 @@ spc_file_t *read_spc_file(char *filename)
 	int err;
 	int x;
 	Uint8 *ptr;
+	int has_id_tag = 0;
 
 	spc_file_t *spc = malloc(sizeof(spc_file_t));
 	if (spc == NULL) {
@@ -294,7 +627,7 @@ spc_file_t *read_spc_file(char *filename)
 	// Read the whole header into a buffer first, then assign individual values.
 	x = fread(buf, 1, 0x2D, f);
 	if (x != 0x2D) {
-		perror("spc_read_file: fread(header)");
+		perror("spc_read_file::fread(header)");
 		free(spc);
 		return(NULL);
 	}
@@ -304,6 +637,14 @@ spc_file_t *read_spc_file(char *filename)
 
 	printf("Header: [%s]\n", spc->header);
 
+	if (memcmp(spc->header, SPC_HEADER_MAGIC, sizeof(SPC_HEADER_MAGIC)) != 0) {
+		fprintf(stderr, "%s: Invalid header or version.\n", filename);
+		exit(1);
+	}
+
+	if (buf[SPC_TAG_TYPE_OFFSET] == SPC_HAS_ID_TAG)
+		has_id_tag = 1;
+
 	ptr = &buf[SPC_VERSION_OFFSET];
 	spc->version_minor = *ptr;
 
@@ -312,14 +653,18 @@ spc_file_t *read_spc_file(char *filename)
 	ptr++;
 
 	memcpy(&spc->registers.pc, ptr, 2);
-	spc->registers.pc = ntohs(spc->registers.pc);
+	// spc->registers.pc = ntohs(spc->registers.pc);
+	spc->registers.pc = le16toh(spc->registers.pc);
 
 	ptr += 2;
 
 	spc->registers.a = *ptr++;
 	spc->registers.x = *ptr++;
 	spc->registers.y = *ptr++;
+
+	// XXX: Make sure flags are defined in the proper order
 	spc->registers.psw.val = *ptr++;
+
 	spc->registers.sp = *ptr++;
 	spc->registers.reserved[0] = *ptr++;
 	spc->registers.reserved[1] = *ptr++;
@@ -345,7 +690,65 @@ spc_file_t *read_spc_file(char *filename)
 		return(NULL);
 	}
 
+	if (has_id_tag) {
+		err = fseek(f, SPC_ID_TAG_OFFSET, SEEK_SET);
+
+		if (err != 0) {
+			perror("spc_read_file::fseek(SPC_ID_TAG_OFFSET)");
+			free(spc);
+			return(NULL);
+		}
+
+		x = fread(spc->id_tag.song_title, 1, SPC_TAG_SONG_TITLE_LEN, f);
+		if (x != SPC_TAG_SONG_TITLE_LEN) {
+			perror("spc_read_file::fread(id_tag.song_title)");
+			free(spc);
+			return(NULL);
+		}
+
+		spc->id_tag.song_title[SPC_TAG_SONG_TITLE_LEN] = '\0';
+
+		x = fread(spc->id_tag.game_title, 1, SPC_TAG_GAME_TITLE_LEN, f);
+		if (x != SPC_TAG_GAME_TITLE_LEN) {
+			perror("spc_read_file::fread(id_tag.game_title)");
+			free(spc);
+			return(NULL);
+		}
+
+		spc->id_tag.song_title[SPC_TAG_GAME_TITLE_LEN] = '\0';
+
+		printf("Song title: %s\n", spc->id_tag.song_title);
+		printf("Game title: %s\n", spc->id_tag.game_title);
+
+		spc->id_tag.dumper[0] = '\0';
+		spc->id_tag.comments[0] = '\0';
+	} else {
+		spc->id_tag.song_title[0] = '\0';
+		spc->id_tag.game_title[0] = '\0';
+		spc->id_tag.dumper[0] = '\0';
+		spc->id_tag.comments[0] = '\0';
+	}
+
 	return(spc);
+}
+
+void dump_mem_line(spc_state_t *state, Uint16 addr) {
+	int x;
+
+	printf("$%04X", addr);
+
+	for (x = 0; x < 16; x++) {
+		printf(" %02X ", state->ram[addr + x]);
+	}
+
+	printf("\n");
+}
+
+void dump_mem(spc_state_t *state, Uint16 addr) {
+	int i;
+
+	for (i = 0; i < 4; i++)
+		dump_mem_line(state, addr + i * 16);
 }
 
 void usage(char *argv0)
@@ -353,11 +756,24 @@ void usage(char *argv0)
 	printf("Usage: %s <filename.spc>\n", argv0);
 }
 
+void show_menu(void) {
+	printf("b <addr>   Set breakpoint on <addr> (ie, \"b abcd\")\n");
+	printf("c          Continue execution\n");
+	printf("d [<addr>] Disassemble at $<addr>, or $pc if addr is not supplied (ie, \"d abcd\")\n");
+	printf("h          Shows this help\n");
+	printf("i          Show registers\n");
+	printf("n          Execute next instruction\n");
+	printf("x <mem>    Examine memory at $<mem> (ie, \"x abcd\")\n");
+}
+
 int main (int argc, char *argv[])
 {
 	spc_file_t *spc_file;
-	Uint8 byte;
 	spc_state_t state;
+	char input[200];
+	int quit = 0;
+	int do_break = 1;
+	int break_addr = -1;
 
 	if (argc != 2) {
 		usage(argv[0]);
@@ -377,6 +793,7 @@ int main (int argc, char *argv[])
 	state.cycle = 0;
 	state.dsp_registers = spc_file->dsp_registers;
 
+	/*
 	dump_registers(state.regs);
 
 	byte = state.ram[state.regs->pc];
@@ -386,11 +803,112 @@ int main (int argc, char *argv[])
 	for (x = 0; x < 100; x++) {
 		inc += dump_instruction(state.regs->pc + inc, state.ram);
 	}
+	*/
 
 	//do_beq(&state, state.ram[state.regs->pc + 1]);
 	//dump_registers(state.regs);
 
-	dump_instruction(state.regs->pc, state.ram);
+	// dump_instruction(state.regs->pc, state.ram);
+
+	while (! quit) {
+		if (state.regs->pc == break_addr) {
+			printf("Reached breakpoint %04X\n", break_addr);
+			do_break = 1;
+		}
+
+		// Should we break after every instruction?
+		if (do_break) {
+			dump_registers(state.regs);
+			dump_instruction(state.regs->pc, state.ram);
+
+			printf("> ");
+			fflush(stdout);
+			char *err = fgets(input, sizeof(input) - 1, stdin);
+
+			if (err == NULL) {
+				quit = 1;
+				break;
+			}
+
+			switch(input[0]) {
+				case '?':
+					show_menu();
+					break;
+
+				case 'b': // break
+				{
+					char *ptr = strchr(input, ' ');
+
+					if (ptr) {
+						break_addr = (Uint16) strtol(ptr, NULL, 16);
+						printf("Breakpoint enabled at %04X\n", break_addr);
+					} else {
+						fprintf(stderr, "Missing argument\n");
+					}
+				}
+				break;
+
+				case 'c': // continue
+					printf("Continue.\n");
+					do_break = 0;
+					break;
+
+				case 'd': // disasm
+				{
+					Uint16 addr;
+					char *ptr = strchr(input, ' ');
+
+					if (ptr) {
+						addr = (Uint16) strtol(ptr, NULL, 16);
+						dump_instruction(addr, state.ram);
+					} else {
+						addr = state.regs->pc;
+					}
+
+					dump_instruction(addr, state.ram);
+				}
+				break;
+
+				case 'h':
+					show_menu();
+					break;
+
+				case 'i':
+					dump_registers(state.regs);
+					break;
+
+				case 'n':
+					execute_next(&state);
+					break;
+
+				case 'q':
+					quit = 1;
+					break;
+
+				case 'x':
+				{
+					char *ptr = strchr(input, ' ');
+
+					if (ptr) {
+						Uint16 addr = (Uint16) strtol(ptr, NULL, 16);
+						dump_mem(&state, addr);
+					} else {
+						fprintf(stderr, "Missing argument\n");
+					}
+				}
+				break;
+
+				default:
+					fprintf(stderr, "Unknown command, %c\n", input[0]);
+
+			}
+
+		} else {
+			dump_registers(state.regs);
+			dump_instruction(state.regs->pc, state.ram);
+			execute_next(&state);
+		}
+	}
 
 	return (0);
 }
